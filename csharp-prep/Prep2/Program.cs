271636567385a436498ed92e03211a2934628c51
JamesGassaway <jamgass329@gmail.com>
2025-01-13 19:47:49 -0700

    Prepare 1/13/25

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string percent = "";
+        if (number >= 90)
+        {
+            percent = "A";
+        }
+        else if (number >= 80)
+        {
+            percent = "B";
+        }
+        else if (number >= 70)
+        {
+            percent = "C";
+        }
+        else if (number >= 60)
+        {
+            percent = "D";
+        }
+        else
+        {
+            percent = "F";
+        }
+        Console.WriteLine($"Your final grade is: {percent}");
     }
 }
