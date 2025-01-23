1ff86081bc41022d7bd5d278ff9d553accfc6806
JamesGassaway <jamgass329@gmail.com>
2025-01-22 21:58:18 -0700

    Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        
+        job2._company = "Apple";
+        job2._jobTitle = "Computer Scientist";
+        job2._startYear = 2016;
+        job2._endYear = 2021;
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        
     }
 }
